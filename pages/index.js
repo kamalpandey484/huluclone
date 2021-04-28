10e384e76be7c3c148c50251957b70b760e31e90
kamalpandey484 <kamal.pandey@tothenew.com>
2021-04-28 20:30:25 +0530

    Build the base

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hulu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hulu Clone</h1>
+    </div>
+  )
+}
