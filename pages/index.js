78859dec1b4ec4fc5f81ea4e44a19d5a0bda6f6b
kamalpandey484 <kamal.pandey@tothenew.com>
2021-04-28 21:30:59 +0530

    Header completed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -7,7 +8,11 @@ export default function Home() {
         <title>Hulu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hulu Clone</h1>
+      {/* Header */}
+      <Header />
+      {/* Nav */}
+
+      {/* Main */}
     </div>
   )
 }
