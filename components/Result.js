a5723597b580a50a4ae37148b22d29c42a6d7804
kamalpandey484 <kamal.pandey@tothenew.com>
2021-04-29 18:48:56 +0530

    Created body part

--- components/Result.js
+++ components/Result.js
@@ -1,9 +1,14 @@
-function Result() {
-    return (
-        <div>
-            
-        </div>
-    )
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+
+function Result({ results }) {
+  return (
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl: flex flex-wrap justify-center">
+      {results.map((result) => {
+        return <Thumbnail key={result.id} result={result} />;
+      })}
+    </FlipMove>
+  );
 }
 
-export default Result
+export default Result;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import requests from "../utils/requests";
 
-export default function Home(props) {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -13,22 +13,22 @@ export default function Home(props) {
       </Head>
       <Header />
       <Nav />
-      {/* <Result /> */}
+      <Result results = {results}/>
     </div>
   );
 }
 
-// export async function getServerSideProps(context) {
-//   const genre = context.query.genre;
-//   const request = await fetch(
-//     `https://api.themoviedb.org/3${
-//       requests[genre]?.url || requests.fetchTrending.url
-//     }`
-//   ).then(res => res.json());
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then(res => res.json());
 
-//   return {
-//     props: {
-//       results: request.results
-//     }
-//   }
-// }
+  return {
+    props: {
+      results: request.results
+    }
+  }
+}
